1882b5aa8bcad20e1f6b8d133e4e900b35fa2976
ranamagdi <ranagrida@gmail.com>
2022-05-10 16:52:21 +0200

    init

---
+++ app.py
@@ -0,0 +1,21 @@
+
+from flask import Flask,render_template,request,jsonify
+from chat import get_response
+
+app = Flask(__name__)
+background_scripts = {}
+@app.get("/")
+def indext_get():
+    return render_template("index.html")
+@app.post("/predict")
+def predict ():
+    text = request.get_json().get("message")
+    #TODO: check if text is valid
+    respose = get_response(text)
+    message ={"answer":respose}
+
+    return jsonify(message)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
